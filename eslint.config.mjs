6963bb3dfdd31d177951ab11254370e3cf58ee1e
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-09-02 09:08:01 +0600

    fix(eslint): disable jsx-a11y/iframe-has-title rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,6 +65,7 @@ export default defineConfig([
       'react-hooks/set-state-in-effect': 0,
       'react-hooks/incompatible-library': 0,
 
+      'jsx-a11y/iframe-has-title': 0,
       'jsx-a11y/click-events-have-key-events': 0,
       'jsx-a11y/label-has-associated-control': 0,
       'jsx-a11y/no-static-element-interactions': 0,
